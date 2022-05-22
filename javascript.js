10fb39159354467d8f5bea4bc375bf99f3808597
Ákos Balogh <akos.balogh87@gmail.com>
2022-05-23 00:07:19 +0200

    Implement basic sketch functionality

--- javascript.js
+++ javascript.js
@@ -1,25 +1,36 @@
 const canvas = document.querySelector('.canvas');
 
-createCanvas(10)
+createCanvas(16)
 
-function createCanvas(size=10){
-    const vw=Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-    const divSize = (vw*6/10 - (size-1))/size;
-    
+
+function createCanvas(size=16){
     if (canvas.hasChildNodes){
         while (canvas.firstChild) {
             canvas.removeChild(canvas.lastChild);
         };
     };
+    const divSize = newDivSize(size);
     for (let i=0; i<size; i++) {
         for (let j=0; j<size; j++){
             const newDiv=document.createElement('div');
             newDiv.classList.add('canvasElement');
-            
             newDiv.setAttribute("style",`width:${divSize}px; height:${divSize}px`);
             canvas.appendChild(newDiv);
         }
     };
 }
 
+function newDivSize(size){
+    const vw=Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    const divSize = (vw*6/10 - (size-1)/2)/size;
+    return divSize;
+}
+
 window.addEventListener('resize', () => {createCanvas(10)},true);
+
+let canvasMouseOver = document.querySelectorAll('.canvasElement');
+canvasMouseOver.forEach((canvasElement) => {
+    canvasElement.addEventListener('mouseenter', () => {
+        canvasElement.style.backgroundColor="black";
+    });
+});
